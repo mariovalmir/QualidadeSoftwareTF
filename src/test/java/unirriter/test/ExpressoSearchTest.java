cb159af955c005f30b5f87381b27d2556a485601
mariovalmir <mario.pereira@acad.pucrs.br>
2017-03-25 10:26:01 -0300

    teste

--- src/test/java/unirriter/test/ExpressoSearchTest.java
+++ src/test/java/unirriter/test/ExpressoSearchTest.java
@@ -17,7 +17,7 @@ public class ExpressoSearchTest {
 
 	@Before
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.chrome.driver", "libs/chromedriver");
+//		System.setProperty("webdriver.chrome.driver", "libs/chromedriver");
 	}
 
 	@Test
